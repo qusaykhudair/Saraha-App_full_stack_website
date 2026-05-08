fa43ebfcb1bba10a2849595cf68d21e0646f3e38
Eng:Qusay khudair <130990177+qusaykhudair@users.noreply.github.com>
2026-05-08 12:22:09 +0300

    feat

--- Backend/src/common/utils/email.utils.js
+++ Backend/src/common/utils/email.utils.js
@@ -2,36 +2,35 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
     if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-        throw new Error("EMAIL_USER or EMAIL_PASS environment variables are missing in Vercel settings!");
+        console.error("❌ EMAIL_USER or EMAIL_PASS environment variables are missing!");
+        return;
     }
 
     // Create a nodemailer transporter
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        service: "gmail",
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: "engqusaykhudair@gmail.com",
+            pass: "sthl ttcu svwv kldg"
         }
     });
 
     // Define the email options
     const mailOptions = {
-        from: `"Saraha App" <${process.env.EMAIL_USER}>`,
+        from: `"Saraha App" <Admin <engqusaykhudair@gmail.com>>`,
         to,
         subject,
         html,
     };
 
     // Send the email
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log(`✅ Email successfully sent to ${to}. MessageId: ${info.messageId}`);
-    } catch (error) {
-        console.error(`❌ Failed to send email to ${to}. Error: ${error.message}`);
-        console.log(`[DEBUG] EMAIL_USER: ${process.env.EMAIL_USER}`);
-        // console.log(`[DEBUG] EMAIL_PASS: ${process.env.EMAIL_PASS ? "****" : "MISSING"}`);
-        console.log(`[DEVELOPMENT PROMPT] Since email failed, here is the content that was supposed to be sent:\n${html}`);
-    }
+    // try {
+    //     const info = await transporter.sendMail(mailOptions);
+    //     console.log(`✅ Email successfully sent to ${to}. MessageId: ${info.messageId}`);
+    // } catch (error) {
+    //     console.error(`❌ Failed to send email to ${to}. Error: ${error.message}`);
+    //     console.log(`[DEBUG] EMAIL_USER: ${process.env.EMAIL_USER}`);
+    //     // console.log(`[DEBUG] EMAIL_PASS: ${process.env.EMAIL_PASS ? "****" : "MISSING"}`);
+    //     console.log(`[DEVELOPMENT PROMPT] Since email failed, here is the content that was supposed to be sent:\n${html}`);
+    // }
 };
